a473cfea115a256459893092ed051e9f22f422bb
Raphael Thanner <r.thanner@zeroseven.de>
2020-09-10 23:05:01 +0200

    Update language wrap viewHelper

--- Classes/ViewHelpers/Language/WrapViewHelper.php
+++ Classes/ViewHelpers/Language/WrapViewHelper.php
@@ -3,32 +3,51 @@
 namespace Zeroseven\Z7BlogComments\ViewHelpers\Language;
 
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 use Zeroseven\Z7BlogComments\Service\LanguageService;
 
-class WrapViewHelper extends AbstractViewHelper
+class WrapViewHelper extends AbstractTagBasedViewHelper
 {
-
-    protected $escapeOutput = false;
-
-    protected $escapeChildren = false;
 
     public function initializeArguments(): void
     {
+        $this->registerTagAttribute('class', 'string', 'CSS class(es) for this element');
+        $this->registerTagAttribute('dir', 'string', 'Text direction for this HTML element. Allowed strings: "ltr" (left to right), "rtl" (right to left)');
+        $this->registerTagAttribute('id', 'string', 'Unique (in this file) identifier for this HTML element.');
+        $this->registerTagAttribute('style', 'string', 'Individual CSS styles for this element');
+        $this->registerTagAttribute('title', 'string', 'Tooltip text of element');
+        $this->registerTagAttribute('accesskey', 'string', 'Keyboard shortcut to access this element');
+        $this->registerTagAttribute('tabindex', 'integer', 'Specifies the tab order of this element');
+
         $this->registerArgument('languageCode', 'string', 'The language attribute text content');
-        $this->registerArgument('tag', 'string', 'Define tag name', false, 'div');
+        $this->registerArgument('tagName', 'string', 'Define tag name');
+        $this->registerArgument('string', 'string', 'Content');
     }
 
-    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext): string
+    public function render()
     {
 
+        // Define content
+        $content = trim($this->arguments['string'] ?: $this->renderChildren());
+
         // Do nothing
-        if(empty($arguments['languageCode']) || $arguments['languageCode'] === LanguageService::getLanguageCode()) {
-            return $renderChildrenClosure();
+        if(($this->arguments['languageCode'] === LanguageService::getLanguageCode() || empty($this->arguments['languageCode'])) && empty($this->arguments['tagName']) && empty($this->arguments['class']) && empty($this->arguments['dir']) && empty($this->arguments['id']) && empty($this->arguments['style']) && empty($this->arguments['title']) && empty($this->arguments['accesskey']) && empty($this->arguments['tabindex'])) {
+            return $content . '<h1>JO!!!</h1>';
         }
 
-        // Wrap text with lang attribute
-        return '<' . $arguments['tag'] . ' lang="' .  $arguments['languageCode'] . '">' .  trim($renderChildrenClosure()) . '</' . $arguments['tag'] . '>';
+        // Set the tag name
+        $this->tag->setTagName($this->arguments['tagName'] ?: 'div');
+
+        // Add language attribute
+        if(!($this->arguments['languageCode'] === LanguageService::getLanguageCode() || empty($this->arguments['languageCode']))) {
+            $this->tag->addAttribute('lang', $this->arguments['languageCode']);
+        }
+
+        // Set content
+        $this->tag->setContent($content);
+
+        // Return the tag
+        return $this->tag->render();
     }
 
 }
