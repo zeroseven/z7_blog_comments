d8cd3e7e14941431d884d8b12a4cd7bae2d939d1
Raphael Thanner <r.thanner@zeroseven.de>
2020-09-10 12:26:48 +0200

    Add simple a captcha

---
+++ Classes/Domain/FormElements/Z7BlogCommentsCaptcha.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Zeroseven\Z7BlogComments\Domain\FormElements;
+
+use TYPO3\CMS\Form\Domain\Model\FormElements\AbstractFormElement;
+use Zeroseven\Z7BlogComments\Service\EncryptionService;
+
+class Z7BlogCommentsCaptcha extends AbstractFormElement
+{
+
+    public function initializeFormElement()
+    {
+
+        // Load "original" method
+        parent::initializeFormElement();
+
+        // Get encryption key
+        $encryptedKey = EncryptionService::encryptKey();
+
+        // Set to data attribute
+        $this->setProperty('title', 'hey');
+        $this->properties['fluidAdditionalAttributes'] = array_merge($this->properties['fluidAdditionalAttributes'] ?? [], ['data-encrypted-key' => $encryptedKey]);
+    }
+
+}
---
+++ Classes/Service/EncryptionService.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Zeroseven\Z7BlogComments\Service;
+
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+class EncryptionService
+{
+
+    /**
+     * Generate an key depending on current page.
+     * The key will be changed daily.
+     *
+     * @return string
+     */
+    protected static function generateKey(): string
+    {
+        if ($GLOBALS['TSFE'] instanceof TypoScriptFrontendController) {
+            return md5(json_encode((array)$GLOBALS['TSFE']->page) . date('Y-m-d'));
+        }
+
+        return md5(__FILE__ . date('Y-m-d'));
+    }
+
+    /**
+     * Returns an base64 encoded and reversed string.
+     *
+     * Same function in JavaScript:
+     * const encryptKey = string => btoa(string.split('').reverse().join(''));
+     *
+     * @return string
+     */
+    public static function encryptKey(): string
+    {
+        return base64_encode(strrev(self::generateKey()));
+    }
+
+    /**
+     * Decrypt the encrypted key
+     *
+     * Same function in JavaScript:
+     * const decryptKey = string => atob(string).split('').reverse().join('');
+     *
+     * @param string
+     * @return string
+     */
+    public static function decryptKey(string $string): string
+    {
+        return base64_decode(strrev($string));
+    }
+
+    /**
+     * Checks if the given string is equal to the generated Key
+     *
+     * @param string $string
+     * @return bool
+     */
+    public static function isValidKey(string $string): bool
+    {
+        return $string === self::generateKey();
+    }
+
+}
