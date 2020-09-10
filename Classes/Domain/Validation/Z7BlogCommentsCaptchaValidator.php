acc988289f848c080ec2340651fbadf1318b8c48
Raphael Thanner <r.thanner@zeroseven.de>
2020-09-10 13:47:46 +0200

    Register custom captcha validator

---
+++ Classes/Domain/Validation/Z7BlogCommentsCaptchaValidator.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Zeroseven\Z7BlogComments\Domain\Validation;
+
+
+use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
+use Zeroseven\Z7BlogComments\Service\EncryptionService;
+
+class Z7BlogCommentsCaptchaValidator extends AbstractValidator {
+
+    protected $acceptsEmptyValues = false;
+
+    protected function isValid($value): void
+    {
+
+        if(!EncryptionService::isValidKey($value)) {
+            $this->addError(
+                $this->translateErrorMessage(
+                    'validation.error.Z7BlogCommentsCaptchaValidator',
+                    'z7_blog_comments'
+                ),
+                1599736767
+            );
+        }
+
+    }
+}
+
