a8902be71168cea1f694207207adada1a044b41a
Raphael Thanner <r.thanner@zeroseven.de>
2020-09-15 22:32:38 +0200

    Control comments in widget by TYPO3 backend ajax API

--- Classes/Widgets/PendingComments.php
+++ Classes/Widgets/PendingComments.php
@@ -9,12 +9,10 @@ use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface as Cache;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\ButtonProviderInterface;
-use TYPO3\CMS\Dashboard\Widgets\ListDataProviderInterface;
 use TYPO3\CMS\Dashboard\Widgets\RequireJsModuleInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
-use Zeroseven\Z7BlogComments\Domain\Demand\CommentDemand;
 use Zeroseven\Z7BlogComments\Domain\Repository\CommentRepository;
 
 class PendingComments implements WidgetInterface, RequireJsModuleInterface
--- Resources/Public/JavaScript/Backend/PendingCommentsWidget.js
+++ Resources/Public/JavaScript/Backend/PendingCommentsWidget.js
@@ -2,12 +2,122 @@
  * Module: TYPO3/CMS/Z7BlogComments/Backend/PendingCommentsWidget
  */
 require(['TYPO3/CMS/Core/Ajax/AjaxRequest'], function (AjaxRequest) {
-  // Generate a random number between 1 and 32
-  const randomNumber = Math.ceil(Math.random() * 32);
 
-  new AjaxRequest(TYPO3.settings.ajaxUrls.example_dosomething).withQueryArguments({input: randomNumber}).get().then(async function (response) {
-    const resolved = await response.resolve();
-    console.log(resolved.result);
-  });
+  var PendingComments = {};
 
+  /** @var string */
+  PendingComments.tableName = 'tx_z7blog_domain_model_comment';
+
+  /**
+   * Handle requests
+   *
+   * @param uid
+   * @param updateFields
+   * @param commands
+   * @param callback
+   * @return bool
+   */
+  PendingComments.request = function (uid, updateFields, commands, callback) {
+
+    // Define arguments
+    var queryArguments = {'data': {}};
+    queryArguments.data[PendingComments.tableName] = {};
+    queryArguments.data[PendingComments.tableName][uid] = {
+      'pending': 0
+    };
+
+    // Add fields to update
+    if(updateFields && typeof updateFields === 'object') {
+      Object.keys(updateFields).forEach(key => {
+        queryArguments.data[PendingComments.tableName][uid][key] = updateFields[key];
+      });
+    }
+
+    // Add commands
+    if (commands && typeof commands === 'object') {
+      queryArguments = {'cmd': {}};
+      queryArguments.cmd[PendingComments.tableName] = {};
+      queryArguments.cmd[PendingComments.tableName][uid] = {};
+
+      Object.keys(commands).forEach(key => {
+        queryArguments.cmd[PendingComments.tableName][uid][key] = commands[key];
+      });
+    }
+
+    // Process request
+    new AjaxRequest(TYPO3.settings.ajaxUrls.record_process).withQueryArguments(queryArguments).get().then(async function (response) {
+      const resolved = await response.resolve();
+
+      // Call callback function
+      if(typeof callback === 'function') {
+        callback(resolved);
+      }
+
+      // Return state
+      return resolved.hasErrors;
+    });
+  };
+
+  /**
+   * Enable comment
+   *
+   * @param uid
+   * @param element
+   * @return bool
+   */
+  PendingComments.enable = function (uid, element) {
+    var target = element || window.event.target;
+    return PendingComments.request(parseInt(uid), {hidden: 0}, null, function() {
+      PendingComments.removeItem(target);
+    });
+  };
+
+  /**
+   * Reject comment
+   *
+   * @param uid
+   * @param element
+   * @return bool
+   */
+  PendingComments.reject = function (uid, element) {
+    var target = element || window.event.target;
+    return PendingComments.request(parseInt(uid), null, null, function() {
+      PendingComments.removeItem(target);
+    });
+  };
+
+  /**
+   * Delete comment
+   *
+   * @param uid
+   * @param element
+   * @return bool
+   */
+  PendingComments.delete = function (uid, element) {
+    var target = element || window.event.target;
+    return PendingComments.request(parseInt(uid), null, {delete: 1}, function() {
+      PendingComments.removeItem(target);
+    });
+  };
+
+  /**
+   * Remove item
+   *
+   * @param target
+   */
+  PendingComments.removeItem = function (target) {
+    var item = target ? target.closest('.js-pending-comment-item') : null;
+
+    if(item) {
+      item.parentNode.removeChild(item);
+    }
+  };
+
+  // Add class to the context
+  TYPO3 = TYPO3 || {};
+  TYPO3.Blog = TYPO3.Blog || {};
+  TYPO3.Blog.PendingComments = PendingComments;
+
+  // Return the object
+  return PendingComments;
 });
