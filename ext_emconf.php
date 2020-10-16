e1b08c0b817c33a53591d2bc3bad1663ff210e3e
Raphael Thanner <r.thanner@zeroseven.de>
2020-10-16 11:56:18 +0000

    Release version 1.0.0 🎉

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,14 +7,14 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Raphael Thanner',
     'author_email' => 'r.thanner@zeroseven.de',
     'author_company' => 'zeroseven design studios GmbH',
-    'state' => 'beta',
+    'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.0-RC1',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
             'form' => '9.5.0-10.4.99',
-            'z7_blog' => ''
+            'z7_blog' => '1.0.0-1.0.99'
         ]
     ]
 ];
