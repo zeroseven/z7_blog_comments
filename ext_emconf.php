cc2704610f189b4853966875c28949a8f9e1a4e8
Sebastian Rosskopf <s.rosskopf@zeroseven.de>
2021-01-04 11:16:43 +0000

    Compatibility update to z7_blog version 1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,12 +9,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'zeroseven design studios GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
             'form' => '9.5.0-10.4.99',
-            'z7_blog' => '1.0.0-1.0.99'
+            'z7_blog' => '1.0.0-1.1.99'
         ]
     ]
 ];
